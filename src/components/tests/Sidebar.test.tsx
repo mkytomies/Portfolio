c911dfa335f026d908dbcb3de262d7cc61b65b61
mkytomies <moonakytomies@gmail.com>
2026-03-13 12:22:41 +0200

    Refactor testing

--- src/components/tests/Sidebar.test.tsx
+++ src/components/tests/Sidebar.test.tsx
@@ -1,60 +1,58 @@
-import { render, screen, renderHook, act, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Sidebar from "../Sidebar";
 import userEvent from "@testing-library/user-event";
-import React, { useState } from "react";
 
-test('Render sidebar on desktop without arrow', async () => {
-    global.innerWidth = 1800;
-    global.dispatchEvent(new Event('resize'));
+describe('Sidebar on desktop', () => {
+    beforeEach(() => {
+        render(
+            <Sidebar />
+        );
+    });
 
-    await act( async () => render(
-        <Sidebar />
-    ));
+    test('render sidebar', () => {
+        expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+        expect(screen.getByText('Skills')).toBeInTheDocument();
+        expect(screen.getByText('Contact & Socials')).toBeInTheDocument();
+    });
 
-    expect(screen.getByTestId('sidebar')).toBeInTheDocument();
-    expect(screen.getByText('Skills')).toBeInTheDocument();
-    expect(screen.getByText('Contact & Socials')).toBeInTheDocument();
-
-    const arrow = screen.getByTestId('arrow');
-    const styles = getComputedStyle(arrow);
-    expect(styles.visibility).toBe('hidden');
+    test('arrow should not be visible', () => {
+        const arrow = screen.getByTestId('arrow');
+        const styles = getComputedStyle(arrow);
+        expect(styles.visibility).toBe('hidden');
+    });
 });
 
-test('Render sidebar on mobile with arrow', async () => {
-    global.innerWidth = 500;
-    global.dispatchEvent(new Event('resize'));
+describe('Sidebar on mobile', () => {
+    beforeEach(() => {
+        global.innerWidth = 500;
+        global.dispatchEvent(new Event('resize'));
 
-    await act( async () => render(
-        <Sidebar />
-    ));
+        render(
+            <Sidebar />
+        );
+    });
 
-    const arrow = screen.getByTestId('arrow');
-    const styles = getComputedStyle(arrow);
-    expect(styles.rotate).toBe('0deg');
-});
+    test('render sidebar with arrow', async () => {
+        const arrow = screen.getByTestId('arrow');
+        const styles = getComputedStyle(arrow);
+        expect(styles.visibility).toBe('visible');
+    });
 
-test('Sidebar should be closed by default on mobile', () => {
-    render(
-        <Sidebar />
-    );
+    test('sidebar should be closed by default on mobile', () => {
+        const sidebarContainer = screen.getByTestId('sidebarContainer');
+        const styles = getComputedStyle(sidebarContainer);
+        expect(styles.left).toBe('-74%');
+    });
 
-    const sidebarContainer = screen.getByTestId('sidebarContainer');
-    const styles = getComputedStyle(sidebarContainer);
-    expect(styles.left).toBe('-74%');
-});
+    test('sidebar should open and close when user click arrow on mobile', async () => {
+        await userEvent.click(screen.getByAltText('Right pointing arrow'));
 
-test('Sidebar should open and close when user click arrow on mobile', async () => {
-    render(
-        <Sidebar />
-    );
+        const sidebarContainer = screen.getByTestId('sidebarContainer');
+        const styles = getComputedStyle(sidebarContainer);
+        expect(styles.left).toBe('0px');
 
-    await userEvent.click(screen.getByAltText('Right pointing arrow'));
-
-    const sidebarContainer = screen.getByTestId('sidebarContainer');
-    const styles = getComputedStyle(sidebarContainer);
-    expect(styles.left).toBe('0px');
-
-    await userEvent.click(screen.getByAltText('Left pointing arrow'));
-    const updatedStyles = getComputedStyle(sidebarContainer);
-    expect(updatedStyles.left).toBe('-74%');
+        await userEvent.click(screen.getByAltText('Left pointing arrow'));
+        const updatedStyles = getComputedStyle(sidebarContainer);
+        expect(updatedStyles.left).toBe('-74%');
+    });
 });
