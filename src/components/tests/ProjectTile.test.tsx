df1f5fe6b18181b3f20415ec964ab2059dca850b
mkytomies <moonakytomies@gmail.com>
2026-03-13 12:21:31 +0200

    Refactor testing

--- src/components/tests/ProjectTile.test.tsx
+++ src/components/tests/ProjectTile.test.tsx
@@ -4,42 +4,76 @@ import ProjectTile from "../ProjectTile";
 
 const name = "Test Project";
 const image = "https://test.url";
+const imageAlt = "Image alt";
 const description = "This is a test project";
 const gitHub = "www.github.com";
 const figma = "www.figma.com";
 
-test('Render project tile', () => {
-    render(
-        <MemoryRouter>
-            <ProjectTile
-                name={name}
-                image={image}
-                description={description}
-                gitHub={gitHub}
-                figma={figma}
-            />
-        </MemoryRouter>
-    );
+describe('Project tile with figma button', () => {
+    beforeEach(() => {
+        render(
+            <MemoryRouter>
+                <ProjectTile
+                    name={name}
+                    image={image}
+                    imageAlt={imageAlt}
+                    description={description}
+                    gitHub={gitHub}
+                    figma={figma}
+                />
+            </MemoryRouter>
+        );
+    });
 
-    const projectTile = screen.getByTestId("projectTile");
-    const projectImage = screen.getByTestId('projectImage');
-    expect(projectTile).toBeInTheDocument();
-    expect(screen.getByText(name).textContent).toBe(name);
-    expect(projectImage.getAttribute('src')).toBe(image);
-    expect(screen.getByText(description).textContent).toMatch(description);
+    test('render project tile', () => {
+        expect(screen.getByRole('article')).toBeInTheDocument();
+    });
+
+    test('render h2 title with the project name', () => {
+        const title = screen.getByRole('heading', {level: 2});
+        expect(title).toHaveTextContent(name);
+    });
+
+    test('render image with the correct src', () => {
+        const projectImage = screen.getByAltText(imageAlt);
+        expect(projectImage).toHaveAttribute('src', image);
+    });
+
+    // NOTE: maybe redundant
+    test('render project description', () => {
+        expect(screen.getByText(description)).toHaveTextContent(description);
+    });
+
+    test('render both buttons', () => {
+        const buttons = screen.getAllByRole('link');
+        expect(buttons).toHaveLength(2);
+    });
+
+    test('github button has the correct url', () => {
+        const githubButton: HTMLAnchorElement = screen.getByText('GitHub');
+        expect(githubButton.href).toContain(gitHub);
+    });
+
+    test('figma button has the correct url', () => {
+        const figmaButton: HTMLAnchorElement = screen.getByText('Figma');
+        expect(figmaButton.href).toContain(figma);
+    });
 });
 
-test('No Figma link displayed when project does not have one', () => {
-    render(
-        <MemoryRouter>
-            <ProjectTile 
-                name={name}
-                image={image}
-                description={description}
-                gitHub={gitHub}
-            />
-        </MemoryRouter>
-    );
+describe('Project tile without Figma button', () => {
+    test('does not render Figma button', () => {
+        render(
+            <MemoryRouter>
+                <ProjectTile
+                    name={name}
+                    image={image}
+                    imageAlt={imageAlt}
+                    description={description}
+                    gitHub={gitHub}
+                />
+            </MemoryRouter>
+        );
 
-    expect(screen.queryByText('Figma')).not.toBeInTheDocument();
+        expect(screen.queryByText('Figma')).not.toBeInTheDocument();
+    });
 });
