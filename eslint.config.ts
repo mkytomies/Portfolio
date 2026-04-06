7839855a4781e623d12b8b55eb5455801b1ac0fd
mkytomies <moonakytomies@gmail.com>
2026-04-06 15:18:00 +0300

    Install and config Eslint and Prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,3 @@
-import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
@@ -6,9 +5,7 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { 
-    files: ["**/*.{ts,tsx}"], 
-    plugins: { js }, 
-    extends: ["js/recommended"], 
+    files: ["**/*.{ts,tsx}"],  
     languageOptions: { globals: globals.browser }
   },
   tseslint.configs.recommended,
